528ea15f80dcad9c8d8ad0aa12fa3a85eb28fc32
kostya-php <kostya.php@gmail.com>
2014-06-11 21:43:03 +0400

    update (наработки графики2)

--- enumerator/table.Designer.cs
+++ enumerator/table.Designer.cs
@@ -28,27 +28,27 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.button1 = new System.Windows.Forms.Button();
-            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
-            // button1
+            // comboBox1
             // 
-            this.button1.Location = new System.Drawing.Point(697, 527);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(12, 529);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(215, 21);
+            this.comboBox1.TabIndex = 2;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
             // 
-            // textBox1
+            // label1
             // 
-            this.textBox1.Location = new System.Drawing.Point(697, 501);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(75, 20);
-            this.textBox1.TabIndex = 1;
-            this.textBox1.Text = "6";
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 513);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(46, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Турнир:";
             // 
             // table
             // 
@@ -56,15 +56,13 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.Control;
             this.ClientSize = new System.Drawing.Size(784, 562);
-            this.Controls.Add(this.textBox1);
-            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.comboBox1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
-            this.MaximizeBox = false;
-            this.MaximumSize = new System.Drawing.Size(800, 600);
-            this.MinimumSize = new System.Drawing.Size(800, 600);
             this.Name = "table";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "table";
+            this.Load += new System.EventHandler(this.table_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -72,7 +70,7 @@
 
         #endregion
 
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label1;
     }
 }
--- enumerator/table.cs
+++ enumerator/table.cs
@@ -8,6 +8,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.Drawing.Imaging;
+using MySql.Data.MySqlClient;
 
 namespace enumerator
 {
@@ -17,32 +18,148 @@ namespace enumerator
         {
             InitializeComponent();
         }
-
-        private void button1_Click(object sender, EventArgs e)
+        private void draw_table(int t)
         {
+            MySqlConnection connect = null;
             try
             {
-                int players = Convert.ToInt32(textBox1.Text);
+                connect = new MySqlConnection(Data.connectionString);
+                connect.Open();
+                string query = "SELECT Count(*) FROM in_tournament WHERE tournament='" + t.ToString() + "'";
+                MySqlCommand cmd = new MySqlCommand(query, connect);
+                int players = Convert.ToInt32(cmd.ExecuteScalar());
+                //int players = Convert.ToInt32(textBox1.Text);
+                int p_width = 200;
+                int res_size = 50;
                 Pen pen1 = new Pen(Color.Black, 3);
                 Graphics g = this.CreateGraphics();
                 g.Clear(Color.FromArgb(0xFF, 0xF0, 0xF0, 0xF0));
                 // Горизонтальные линии
+                g.DrawLine(pen1, 50, 50, 50 + p_width + (res_size * 2) + (res_size * players), 50);
+                g.DrawLine(pen1, 50, 50 + res_size, 50 + p_width + (res_size * 2) + (res_size * players), 50 + res_size);
                 for (int i = 1; i <= players; i++)
                 {
-                    g.DrawLine(pen1, 50, 50 * i, 50 * players, 50 * i);
+                    g.DrawLine(pen1, 50, 50 + res_size + (res_size * i), 50 + p_width + (res_size * 2) + (res_size * players), 50 + res_size + (res_size * i));
                 }
                 // Вертикальные линии
+                g.DrawLine(pen1, 50, 50, 50, 50 + res_size + (res_size * players));
+                g.DrawLine(pen1, 50 + p_width, 50, 50 + p_width, 50 + res_size + (res_size * players));
                 for (int i = 1; i <= players; i++)
                 {
-                    g.DrawLine(pen1, 50 * i, 50, 50 * i, 50 * players);
+                    g.DrawLine(pen1, 50 + p_width + (res_size * i), 50, 50 + p_width + (res_size * i), 50 + res_size + (res_size * players));
+                }
+                g.DrawLine(pen1, 50 + p_width + res_size + (res_size * players), 50, 50 + p_width + res_size + (res_size * players), 50 + res_size + (res_size * players));
+                g.DrawLine(pen1, 50 + p_width + (res_size * 2) + (res_size * players), 50, 50 + p_width + (res_size * 2) + (res_size * players), 50 + res_size + (res_size * players));
+
+                // Create font and brush.
+                Font drawFont = new Font("Arial", 16);
+                SolidBrush drawBrush = new SolidBrush(Color.Black);
+
+                // Create point for upper-left corner of drawing.
+                //PointF drawPoint = new PointF(150.0F, 50.0F);
+                Point drawPoint = new Point(105, 65);
+
+                // Set format of string.
+                StringFormat drawFormat = new StringFormat();
+                drawFormat.FormatFlags = StringFormatFlags.NoWrap;
+
+                // Draw string to screen.
+                g.DrawString("Игроки", drawFont, drawBrush, drawPoint, drawFormat);
+
+                for (int i = 1; i <= players; i++)
+                {
+                    g.DrawString(i.ToString(), drawFont, drawBrush, new Point(p_width + 15 + (50 * i), 65), drawFormat);
+                }
+                g.DrawString("О", drawFont, drawBrush, new Point(p_width + 15 + (50 * (players + 1)), 65), drawFormat);
+                g.DrawString("М", drawFont, drawBrush, new Point(p_width + 15 + (50 * (players + 2)), 65), drawFormat);
+
+                string[] pl = new string[players];
+                query = "SELECT * FROM in_tournament WHERE tournament='"+t.ToString()+"' ORDER BY number ASC";
+                cmd = new MySqlCommand(query, connect);
+                MySqlDataReader dataReader = cmd.ExecuteReader();
+                int k = 0;
+                while (dataReader.Read())
+                {
+                    pl[k] = Data.player_name(Convert.ToInt32(dataReader["player"]));
+                    k++;
+                }
+                /*
+                pl[0] = "Салынский Иван";
+                pl[1] = "Охмак Николай";
+                pl[2] = "Шипка Роман";
+                pl[3] = "Янина Надежда";
+                pl[4] = "Пришвин Константин";
+                pl[5] = "Ткаченко Александр";
+                */
+                for (int i = 1; i <= players; i++)
+                {
+                    g.DrawString(pl[i - 1], new Font("Arial", 14), drawBrush, new Point(50, 65 + (res_size * i)), drawFormat);
+                }
+
+                for (int i = 1; i <= players; i++)
+                {
+                    for (int j = 1; j <= players; j++)
+                    {
+                        // Диагональ
+                        if (i == j)
+                        {
+                            g.DrawString("-", new Font("Arial", 14), drawBrush, new Point(15 + p_width + (50 * i), 65 + (50 * j)), drawFormat);
+                        }
+                        if (i > j)
+                        {
+                            g.DrawString(j.ToString()+":"+i.ToString(), new Font("Arial", 14), drawBrush, new Point(15 + p_width + (50 * i), 65 + (50 * j)), drawFormat);
+                        }
+
+                    }
                 }
                 pen1.Dispose();
                 g.Dispose();
             }
-            catch
+            catch (MySqlException err)
             {
-
+                MessageBox.Show("Ошибка: " + err.ToString());
             }
+            finally
+            {
+                if (connect != null)
+                {
+                    connect.Close();
+                }
+            }
+        }
+
+        private void table_Load(object sender, EventArgs e)
+        {
+            MySqlConnection connect = null;
+            try
+            {
+                connect = new MySqlConnection(Data.connectionString);
+                connect.Open();
+                string query = "SELECT * FROM tournaments ORDER BY id ASC";
+                MySqlCommand cmd = new MySqlCommand(query, connect);
+                MySqlDataReader dataReader = cmd.ExecuteReader();
+                while (dataReader.Read())
+                {
+                    comboBox1.Items.Add(dataReader["name"]);
+                }
+                dataReader.Close();
+            }
+            catch (MySqlException err)
+            {
+                MessageBox.Show("Ошибка: " + err.ToString());
+            }
+            finally
+            {
+                if (connect != null)
+                {
+                    connect.Close();
+                }
+            }
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            draw_table(comboBox1.SelectedIndex + 1);
         }
     }
 }
